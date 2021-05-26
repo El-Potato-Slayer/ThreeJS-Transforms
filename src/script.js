3ceda3f2fde48ebe5cd70dc76957cadfd3d85403
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-05-26 22:01:11 +0400

    practice axes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
 
+
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -13,7 +15,10 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+mesh.position.x = 3
 scene.add(mesh)
+
 
 /**
  * Sizes
@@ -27,8 +32,17 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 5
+camera.position.x = 1
+camera.position.y = 1
 scene.add(camera)
+
+
+/**
+ * Axes Helper
+ */
+ const axesHelper = new THREE.AxesHelper(3)
+ scene.add(axesHelper)
 
 /**
  * Renderer
